a680ce7d2ac781960fe01b615872ae0620fec87e
Andrés Felipe Forero Pérez <149700794+AndresFForeroP@users.noreply.github.com>
2025-02-12 15:45:29 -0500

    Delete prueba.py

--- prueba.py
+++
@@ -1,4 +0,0 @@
-import modules.f_coleccion as fc
-diccionario = fc.leer_json('registros.json')
-diccionario["libros"]['ñoño'] = diccionario['libros'].pop("ñeñe")
-fc.escribir_json('registros.json',diccionario)
